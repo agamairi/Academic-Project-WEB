dbed5c1cd2b6dc283315beaf483d880178e7799b
agamairi <agam.airi@gmail.com>
2022-12-03 22:53:21 -0500

    link update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // EMAIL: aairi@myseneca.ca
 // SUBJECT: web322
 // I have done all this work by myself and have not copied it from anywhere
-// LINK: https://web322-app-a.herokuapp.com/
+// LINK: https://creepy-plum-gosling.cyclic.app/
 
 const express = require("express");
 const path = require("path");
